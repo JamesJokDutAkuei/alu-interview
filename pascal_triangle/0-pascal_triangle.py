372db717ff65649a2a9dea8f35cb6d2a940d25bf
JamesJokDutAkuei <j.akuei@alustudent.com>
2024-10-12 20:17:37 +0000

    file

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
+"""Check readme for full description"""
+
+
 def pascal_triangle(n):
-	if n <= 0:
-		return []
-	triangle = [[1]]
-	for i in range(1, n):
-		row = [1]
-		for j in range(1, i):
-			row.append(triangle[i-1][j-1] + triangle[i-1][j])
-		row.append(1)
-		triangle.append(row)
-	return triangle
-print(pascal_triangle(5))			
+    """ returns a list of lists of integers(Pascals triangle of n)"""
+
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # Start with the first row as [1]
+
+    for i in range(1, n):
+        row = [1]  # Each row starts with 1
+        for j in range(1, i):
+            # Each element is the sum of two elements from the previous row
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)  # Each row ends with 1
+        triangle.append(row)
+
+    return triangle
